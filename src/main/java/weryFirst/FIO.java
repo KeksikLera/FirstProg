c18a99f003c7e67138fc59d9a13ec9e2b21de9b5
Lera <lada.latushka@gmail.com>
2020-04-12 21:51:29 +0300

    create first class

---
+++ src/main/java/weryFirst/FIO.java
@@ -0,0 +1,35 @@
+package weryFirst;
+
+public class FIO {
+    private String FirstName;
+    private String MiddleName;
+    private String LastName;
+
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    public void setFirstName(String fn) {
+        this.FirstName = fn;
+    }
+
+    public String getMiddleName() {
+        return MiddleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        MiddleName = middleName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String lastName) {
+        LastName = lastName;
+    }
+
+    public String getFIO() {
+        return null;
+    }
+}
